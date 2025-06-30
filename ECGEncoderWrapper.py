d96152087bd0497879d2377bb2473576c967f6fd
cgdsym <112564900+cgdsym@users.noreply.github.com>
2025-06-30 18:32:31 +0800

    Add files via upload

--- ECGEncoderWrapper.py
+++ ECGEncoderWrapper.py
@@ -2,18 +2,28 @@ import torch
 import torch.nn as nn
 from xresnet1d_101 import xresnet1d101
 class KEDEncoderWrapper(nn.Module):
-    def __init__(self, ked_encoder, global_dim=512, token_num=100):
+    def __init__(self, ked_encoder, token_num=100):
         super().__init__()
-        self.ked_encoder = ked_encoder                  # 已加载好权重的KED模型
-        self.adapt_pool = nn.AdaptiveAvgPool1d(token_num)  # 将token数变为100
-        self.proj = nn.Linear(768, global_dim)              # 将768维降为512维
+        self.ked_encoder = ked_encoder
+        self.adapt_pool = nn.AdaptiveAvgPool1d(token_num)
+       
 
+        # 记录模型关键配置参数
+        self.hidden_size = 768   # 按你的模型实际输出设定
+        self.token_num = token_num
+
+
+        # 参数全部冻结（不参与训练）
+        for p in self.ked_encoder.parameters():
+            p.requires_grad = False
+    
+
+    @torch.no_grad()  # 前向推理不计算梯度
     def forward(self, x):
         feats = self.ked_encoder(x)           # [batch, 768, 157]
         token_emb = self.adapt_pool(feats)    # [batch, 768, 100]
         token_emb = token_emb.transpose(1,2)  # [batch, 100, 768]
         global_emb = token_emb.mean(dim=1)    # [batch, 768]
-        global_emb = self.proj(global_emb)    # [batch, 512]
         return global_emb, token_emb
 
 if __name__ == '__main__':
@@ -24,5 +34,6 @@ if __name__ == '__main__':
     # forward推理
     x = torch.randn(8, 12, 5000)  # 假设batch=8, 12导联, 5000采样点
     global_emb, token_emb = ecg_encoder(x)
-    print(global_emb.shape)  # torch.Size([8, 512])
+    #global_emb=ecg_encoder(x)
+    print(global_emb.shape)  # torch.Size([8, 768])
     print(token_emb.shape)   # torch.Size([8, 100, 768])
